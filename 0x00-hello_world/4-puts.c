7faf0991a9a4a0cd199e9a1af972518d959d421c
temijopelo <temijeje@gmail.com>
2023-10-05 15:39:15 -0700

    file 04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - is an entry point
+ *Return ; 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
