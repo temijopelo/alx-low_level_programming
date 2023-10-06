48edd458108c49e7cf84c09d8d5767535a381a63
temijopelo <temijeje@gmail.com>
2023-10-05 19:20:32 -0700

    finale file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - entry point
  *
  * Return: 1 error
  */
-
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-for (int i = 0; message[i] != '\0'; i++)
-{
-putchar(message[i]);
-}
-putchar('\n');
+write
+	(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
