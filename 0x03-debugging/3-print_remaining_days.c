32dc47ef03acc68ea1aae2ea87ba22b998b3b540
temijopelo <temijeje@gmail.com>
2023-10-12 20:03:18 -0700

    commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
 	{
-		if (month <= 12)
+		if (month <= 12 && day <= 31)
 		{
 			day++;
 		}
