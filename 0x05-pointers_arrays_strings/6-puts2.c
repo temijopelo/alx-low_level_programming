c69f078d04a9d2423e8c76dc85e70b0269c9b462
temijopelo <temijeje@gmail.com>
2023-10-16 17:55:46 -0700

    project 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,17 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int len = 0;
 
-	while (str[i] != '\0')
+	while (str[len] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		len++;
 	}
+	len -= 1;
+
+	for (; i <= len; i += 2)
+{
+	_putchar(str[i]);
+}
 	_putchar('\n');
 }
