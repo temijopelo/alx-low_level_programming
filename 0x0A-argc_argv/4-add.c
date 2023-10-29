2791f3ad525a09557a2bd035a8a9066f884622b1
temijopelo <temijeje@gmail.com>
2023-10-29 15:55:24 -0700

    task 05

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ * @argc: interger
+ * @argv: string
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num, result = 0, i;
+
+	while (argc-- > 1)
+	{
+		for (i = 0; argv[argc][i]; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
