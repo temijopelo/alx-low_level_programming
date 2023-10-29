99b028a68a665dd9bed56f20484ee29668f7c21e
temijopelo <temijeje@gmail.com>
2023-10-29 12:12:21 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,13 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 1)
-	{
-		printf("no program file found\n");
-	}
-	else
-	{
-		printf("Program name: %s\n", argv[0]);
-	}
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
 	return (0);
 }
