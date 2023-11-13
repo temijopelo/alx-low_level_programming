fc3706f286f008fc37a1a2d698eed631b0eb8e9c
temijopelo <temijeje@gmail.com>
2023-11-13 06:43:37 -0800

    task 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int bytes, i;
-	unsigned char *func_ptr;
+	char *func_ptr;
 
 	if (argc != 2)
 	{
@@ -24,12 +24,14 @@ int main(int argc, char *argv[])
 		exit(2);
 	}
 	func_ptr = (unsigned char *)main;
-	i = 0;
-	if (bytes > 0)
+	for (i = 0; i < bytes; i++)
 	{
-		while (i < (bytes - 1))
-			printf("%02hhx ", func_ptr[i++]);
-		printf("%hhx\n", func_ptr[i]);
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", func_ptr[i]);
+			break;
+		}
+		printf("%02hhx ", func_ptr[i]);
 	}
 	return (0);
 }
