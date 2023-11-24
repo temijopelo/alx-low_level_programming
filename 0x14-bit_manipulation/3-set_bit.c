11a72b23d22d6339c667a65c147216d51a4787d9
temijopelo <temijeje@gmail.com>
2023-11-23 20:26:39 -0800

    task 3 redone

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,11 +8,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
+	unsigned int mask;
 
-	max <<= index;
-	if (max == 0)
+	if (index > sizeof(unsigned int) * 8)
 		return (-1);
-	*n |= max;
+	mask = 1;
+	mask = mask << index;
+	*n = ((*n) | mask);
 	return (1);
 }
